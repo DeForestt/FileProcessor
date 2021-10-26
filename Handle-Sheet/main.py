44172249252d359399a2cefc215b509a29a5f4dd
DeForestt Thompson <deforesttthompson@gmail.com>
2021-10-26 11:43:41 -0400

    added new procs

--- Handle-Sheet/main.py
+++ Handle-Sheet/main.py
@@ -5,6 +5,8 @@ import pandas
 from openpyxl import load_workbook
 from Processes.Lumen import lumenProc
 from Processes.PYProc import PyProc
+from Processes.FedExPhoneProc import FedexPhoneProc
+from Processes.XPOAgingProc import XPOAgingProc
 import threading
 
 
@@ -19,6 +21,12 @@ def main():
     PY = threading.Thread(target=PyProc)
     tPY = False
 
+    fedex = threading.Thread(target=FedexPhoneProc)
+    tFedEx = False
+
+    XPOAging = threading.Thread(target=XPOAgingProc)
+    tXPOAging = False
+
     if exists("LumenInput.xlsx"):
         lumen.start()
         tlumen = True
@@ -26,9 +34,19 @@ def main():
     if exists("PYInput.xlsx"):
         PY.start()
         tPY = True
+    
+    if exists("FedEx-Customer-Phone-Numbers.xlsx"):
+        fedex.start()
+        tFedEx = True
+
+    if exists("AGING-WITH-ADDED-COLUMNS.TXT") and exists("IN-AGING-NOT-IN-SYSTEM.TXT") and exists("IN-SYSTEM-NOT-IN-AGING.TXT"):
+        XPOAging.start()
+        tXPOAging = True
 
     if tlumen: lumen.join()
     if tPY: PY.join()
+    if tFedEx: fedex.join()
+    if tXPOAging: XPOAging.join()
 
     print("All Processes Complete")
 
